3245a96cc087f22e230756944e04b284c5586e63
Krouns <aupa13@NOILAB31352.sapient.com>
2015-09-30 12:26:05 +0530

    asdas

--- Practice/src/com/Example_5.java
+++ Practice/src/com/Example_5.java
@@ -3,7 +3,7 @@ package com;
 class NameRunnable implements Runnable {
 public void run() {
 for (int x = 1; x < 4; x++) {
-System.out.println("Run by " + Thread.currentThread().getName());
+System.out.println("aaRun by " + Thread.currentThread().getName());
    }
   }
  }
